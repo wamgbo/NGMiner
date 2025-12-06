4dfb58bca8a1a7ecd4499edee209148d712228d0
Wang_bo <87056038+wamgbo@users.noreply.github.com>
2025-12-06 21:55:28 +0800

    1.0.0

--- week13.py
+++ week13.py
@@ -1,4 +1,3 @@
-import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 import time
@@ -10,9 +9,10 @@ class webcraw:
         self.url=url+version+'/'
         driver = webdriver.Chrome()
         driver.get(self.url)
-
+        time.sleep(1)
         source=driver.page_source
         self.soup=BeautifulSoup(source,'html.parser')
+        driver.quit()
     def select_version(self):
         return self._select_version()
     def _select_version(self):
@@ -23,7 +23,9 @@ class webcraw:
             return db[driver_list[version]]
         except Exception as e :
             print(e)
-            
+        except KeyboardInterrupt as e:
+            exit()
+            print(e)
     def _print_version(self):
         count=0
         for i,j in enumerate(self._inital_db()):
@@ -32,20 +34,36 @@ class webcraw:
     def _inital_db(self):
         driver_dict = {
             "591.44": '258949',
-            "581.80": '257597'
+            "581.80": '257597',
+            "581.57": '257398',
+            "581.42": '257266',
+            "581.29": '254559',
+            "581.15": '253200'
         }
         return driver_dict
-        
-    def result(self):
-        print(self.soup)
-    
     def info(self):
         web=self.soup
-        title=span = web.select_one('#ddVersionLbl_td span')
-        print(title)
-        
-
-
-
+        for i in web.find_all("a"):
+            i.decompose()
+        title=web.find_all("span",class_="RsltDtlsHeadingCell")
+        content=web.find_all("span",class_="RsltDtlsValueCell")
+        print('='*40)
+        for i in range(0, 5):
+            print(title[i].contents[0], end='')
+            html_block = str(content[i])
+            clean = BeautifulSoup(html_block, "html.parser").get_text(separator="")
+            clean = clean.strip()
+            print(clean)
+        print('='*40)
+        release_notes=web.find("h2",id="lclReleaseNotes")
+        release_notes_div=web.find("div",id="ddReleaseNotes_div")
+        html = "".join(str(x) for x in release_notes_div.contents)#把 <div> 內的所有內容整合成一個 HTML 字串。
+        clean_notes = BeautifulSoup(html, "html.parser").get_text(separator="\n").strip()#將剛剛合併的 HTML 字串解析成 BeautifulSoup 物件。 separator="\n" 代表每個區塊之間用換行符號分開（避免文字黏在一起）。去掉字串開頭和結尾的多餘空白或換行。        
+        lines = [line.strip() for line in clean_notes.splitlines() if line.strip()]#clean_notes.splitlines()將文字按換行符號分割成列表，每一行變成一個元素。
+        print(release_notes.contents)
+        for i in lines:
+            print(i)
+            print()
+        print('='*40)
 craw=webcraw()
 craw.info()
